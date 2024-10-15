b1bb0d93fc72709dd023d4d8779cccd0f32c6edd
saumittras <saumittra.eee@gmail.com>
2024-10-15 22:25:14 +0600

    propes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ function App() {
     <>
       <h1>Vite + React</h1>
       <Mori></Mori>
+      <Device name="Laptop"></Device>
+      <Device></Device>
+      <Device></Device>
       <Person></Person>
+      <Student></Student>
+      <Developer></Developer>
     </>
   );
 }
@@ -23,6 +28,33 @@ function Person() {
       money. i have {money} taka. I am looking for spend money{" "}
     </h3>
   );
+}
+function Student() {
+  return (
+    <div className="student">
+      <h3>This is a student</h3>
+      <p>Name: </p>
+      <p>Age: </p>
+    </div>
+  );
+}
+function Developer() {
+  const developerStyle = {
+    margin: "20px",
+    padding: "20px",
+    border: "2px Solid purple",
+    borderRadius: "10px",
+  };
+  return (
+    <div style={developerStyle}>
+      <h5>Devo Devo</h5>
+      <p>Coding:</p>
+    </div>
+  );
+}
+function Device(props) {
+  console.log(props);
+  return <h2>This Device: Laptop</h2>;
 }
 
 export default App;
